e9ed869d2e469faa6974f5516b5fff3a36a6918d
Osteriux <pixave73@gmail.com>
2022-11-10 18:58:02 +0100

    création d'un fichier test_crud.php contenant des formulaires de test pour la base de donnée

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ $html_header .= "<li><a href='pages/avis_clients.php'>Avis clients</a></li>\n";
 $html_header .= "<li><a href='pages/panier.php'>Panier</a></li>\n";
 $html_header .= "<li><a href='pages/confirmation_commande.php'>Confirmation de commande</a></li>\n";
 $html_header .= "<li><a href='pages/actualites.php'>Actualites</a></li>\n";
+$html_header .= "<li><a href='pages/test_crud.php'>TEST</a></li>\n";
 
 $html_header .= "</ul>\n";
 $html_header .= "</div>\n";
