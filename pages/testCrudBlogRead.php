9902dd23fe6c8cc538a056f5612be1245125a422
Amélie HACQUE <95433250+Ameliorer@users.noreply.github.com>
2022-11-12 00:09:27 +0100

    CRUD blog + pages test CRUD Blog

---
+++ pages/testCrudBlogRead.php
@@ -0,0 +1,103 @@
+<?php
+//Premiere balise PHP pour includes les principaux fichiers (Head,db_connect...) ainsi que pour s'occuper des sessions (visiteur,admin)
+//–––––––––––––––
+
+$html_head = "<!DOCTYPE html>\n";
+$html_head .= "<html>\n<head>\n";
+$html_head .= "<meta charset='utf-8' />\n";
+$html_head .= "<title>test crud</title>\n";
+$html_head .= "</head>\n";
+$html_head .= "<body>\n";
+
+echo $html_head;
+
+//include BDD connect
+include("../db/db_connect.php");
+
+//include cruds
+include("../crud/crud_blog.php");
+
+?>
+
+<?php
+// ---  envoie données BDD  ---
+
+//var_dump($_POST);
+
+//users
+if(isset($_POST["id"])){
+    $id = $_POST["id"];
+}
+
+if(isset($_POST["title"])){
+    $title = $_POST["title"];
+}
+
+if(isset($_POST["content"])){
+    $content = $_POST["content"];
+}
+
+if(isset($_POST["datePost"])){
+    $datePost = $_POST["datePost"];
+}
+
+?>
+
+<p><a href="../index.php">INDEX</a></p>
+
+<div>
+    <div>
+        <h3>SELECT POST</h3>
+        <form action="" method="post" name="FormBlog" id="blogForm" class="form">
+            <label for="id">Id du post : </label>
+            <input type="number" placeholder="id" name="id" id="id"/>
+
+            <label for="submit">valider : </label>
+            <input type="submit" name="submitSelect" id="submit">
+        </form>
+    </div>
+</div>
+
+    <script>
+        console.log("pop");
+        let forms = document.querySelectorAll(".form");
+
+        for(let form of forms){
+            form.reset();
+            console.log(form);
+        }
+    </script>
+
+<?php
+
+if (!empty($_POST["id"])){
+
+    $res = SelectBlog($conn, $id);
+
+
+
+    if(!$res){
+        echo("Something wrong append...<br>Did you complet all the cases ?");
+    }
+    else {
+        foreach($res as $key => $val){
+            echo("<br><li> $key : $res[$key] </li>");}
+
+    }
+}
+
+?>
+
+
+
+<?php
+//Troisieme balise PHP pour include le(s) fichier(s) du footer
+//–––––––––––––––
+
+//include BDD disconnect
+include("../db/db_disconnect.php");
+
+//Include du footer
+include("footer.php");
+
+?>
