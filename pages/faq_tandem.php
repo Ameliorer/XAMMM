77bcc95215a3c5588427405de1bf29505e0d128d
Mata <mat.ribiollet@gmail.com>
2022-11-15 23:02:35 +0100

    FAQ complétée selon les données du PRO

--- pages/faq_tandem.php
+++ pages/faq_tandem.php
@@ -11,11 +11,22 @@ include("head.php");
 //Deuxieme balise PHP : La page
 //–––––––––––––––
 
+
 $html_acceuil = "<div><a href='../index.php'>ACCEUIL</a></div>";
 echo($html_acceuil);
 ?>
 
+<header id="headerFAQ">
+    <h1>FAQ</h1>
+    <h4>Des questions ? Peut-être que les réponses existent déja :</h4>
+</header>
 
+<main>
+    <div id="FAQContent"></div>
+</main>
+
+<script src="../scripts/axios.min.js" ></script> 
+<script src="../scripts/faq.js" defer></script>
 <?php 
 //Troisieme balise PHP pour include le(s) fichier(s) du footer
 //–––––––––––––––
