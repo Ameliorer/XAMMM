a59842b8f5de395d5773794420d8aa8ada2f0d51
UP-4303 <up4303.pro@protonmail.com>
2022-11-17 14:51:44 +0100

    index.php ajout du contenu de accueil.docx

--- index.php
+++ index.php
@@ -45,6 +45,15 @@ echo($html_header);
 
 ?>
 
+<main>
+    <p>Fondée en 1999, l’école professionnelle de parachutisme Diagonale s’est installée sur la plateforme de Gap Tallard pour bénéficier d’une météo exceptionnelle (Pas de mistral et 300 jours de soleil par an), d’un superbe panorama montagnard et d’un espace aérien dédié au parachutisme (pas de ligne commerciale).</p>
+    <p>L’équipe Diagonale peut, dans ces conditions, de mars à novembre, atteindre ses  objectifs : vous faire partager sa passion en toute sécurité.</p>
+    <p>Pierre est moniteur de parachutisme (titulaire du Brevet d'Etat Educateur Sportif 1° degré) depuis 1997. Avec bientôt 20 ans d'activité, il totalise aujourd'hui 21.000 sauts dont près de 16.000 sauts d'enseignement. Plus de 5000 sauts en tandem et plus de  1000 élèves  en stage PAC.</p>
+    <p>Pierre a fait partie de l'équipe des Souls Flyers et participe avec la Fédération Française de Parachutisme à la formation des moniteurs depuis 2002. Vous pouvez sauter avec lui en toute confiance !!!</p>
+    <p>Il totalise aujourd'hui plus de 20.000 sauts et a exploré toutes les facettes du parachutisme : progression, loisir, enseignement, compétition (sky-surf et free-fly), prise de vue professionnelle, sauts de démonstration (Soul flyers), formation au monitorat (avec la fédération Française de Parachutisme), soufflerie (Tunnelinstructor A, A+, B).</p>
+    <p>Avec son experience de 20 ans d'enseignement, il peut vous faire progresser quelque soit votre niveau et la discipline souhaitée (Progression Brevet A, B, B2 (vol relatif), Bi4 (free-fly tête en haut), B4 (free-fly tête en bas), formation au monitorat et soufflerie).</p>
+</main>
+
 <?php 
 //Troisieme balise PHP pour include le(s) fichier(s) du footer
 //–––––––––––––––
