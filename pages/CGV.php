8189a48b93bd534727fd25c575b00eb3c40c74c9
Amélie HACQUE <95433250+Ameliorer@users.noreply.github.com>
2022-11-17 21:01:18 +0100

    création des CGV et ses données

---
+++ pages/CGV.php
@@ -0,0 +1,118 @@
+<?php
+include("head.php");
+?>
+
+<?php
+//Header of
+$html_accueil = "<div><a href='../index.php'>ACCUEIL</a></div>";
+echo($html_accueil);
+?>
+
+<header id="headerCGV">
+    <h1>Conditions Générales de Vente</h1>
+    <p>Les présentes conditions de vente sont conclues d’une part par le site <a href="www.diagonale-chute-libre.com">
+        www.diagonale-chute-libre.com</a> dont le siège social est situé à Hameau
+        Saint Laurent 05130 Sigoyer, entreprise  Pierre Desmet  DIAGONALE CHUTE LIBRE
+        enregistrée sous le SIRET 423 769 991 00070  et, d’autre part, par toute personne
+        physique ou morale souhaitant procéder à un achat via le site internet
+        <a href="www.diagonale-chute-libre.com">www.diagonale-chute-libre.com</a> dénommée ci-après "l'acheteur".
+    </p>
+</header>
+<main>
+    <h2>BAPTEME EN CHUTE LIBRE TANDEM</h2>
+    <p>Ce billet correspond au paiement d’un baptême de chute libre en tandem pour toute personne
+        répondant aux critères suivants :
+    </p>
+    <ul>
+        <li>Age minimum 15 ans révolus, (autorisation écrite des deux parents obligatoire pour les
+            mineurs).
+        </li>
+        <li>Poids maximum 90kg, un contrôle pourra être effectué !
+        </li>
+        <li>Etre en bonne santé et se présenter le jour du saut avec un certificat médical de non
+            contre-indication à la pratique d’un saut en parachute. Ce document doit dater de moins
+            de 6 mois et peut être délivré par un médecin généraliste.
+        </li>
+        <li>Le client qui a consommé de l'alcool ou des substances illicites avant un saut
+            se verra automatiquement refuser l'accès de l'avion.
+        </li>
+        <li>Tenue pour sauter : jeans, survêtement, short, polaire, chaussures de sport et éventuellement gants.
+            Pas de chaussures montantes ou de chaussures à crochet.
+        </li>
+    </ul>
+    <p>Ce billet n’est pas nominatif ; il est non remboursable passé le délai légal de rétractation.
+        Sa validité est de 12 mois date à date selon nos périodes d’activité. <br>
+        Les offres promotionnelles ne sont pas cumulables.<br>
+        Ce billet peut être transmis à toute autre personne répondant aux critères ci-dessus.
+        La prise de rendez-vous par téléphone ou email vaut pour réservation.
+        Prévoir <strong> 3 à 4 heures </strong> sur place. Toute annulation (de la part du détenteur
+        de ce billet ou d’un de ses représentants) du rendez-vous moins de 48 heures avant la date de
+        saut fixée, de non présentation ou le fait de se présenter au rendez-vous en ne
+        remplissant pas l’un des critères ci-dessus entrainera la perte totale de ce billet.
+        <u>Aucun remboursement ni dédommagement ne sera accordé.</u>
+    </p>
+    <p> La réalisation du saut est soumise à des impératifs météorologiques, administratifs et techniques.<br>
+        Si l’encadrement décide que les conditions ne sont pas réunies pour effectuer le saut,
+        ce dernier sera reporté à une date ultérieure. Un report lié à la météorologie ne donne
+        pas droit à une prolongation de la date de validité. Nous vous conseillons donc de fixer
+        une date de saut dès l’achat de ce bon et de ne pas attendre la fin de la date de validité.
+        Moyennant le règlement d’un supplément, vous pouvez, sous certaines conditions, prolonger
+        la validité de votre bon, pour tout renseignement : diagonalechutelibre@gmail.com  ou tél. +33(0)6.60.83.12.84.
+    </p>
+    <p>Les baptêmes de chute libre sont réalisés par des titulaires du Brevet de Parachutistes
+        Professionnels qualification biplace ou du Brevet d’état (BEES/BPJEPS) option tandem.
+        En souhaitant de votre plein gré, participer à cette activité, vous reconnaissez connaitre
+        et accepter les risques liés à la pratique d’une activité aéronautique en générale et du
+        parachutisme en particulier (incluant le baptême de chute libre avec vidéo embarquée ou non,
+        perche ou poignet).
+    </p>
+
+    <h2>VIDEO EN CHUTE</h2>
+    <p>En cas de mauvais fonctionnement de la caméra, seul le montant correspondant à la vidéo sera remboursé. L’emport
+        par des passagers tandem d’appareils photos vidéo ou de téléphone  est strictement interdit.
+    </p>
+
+    <h2>INITIATION ou STAGE PAC</h2>
+    <p>Ce billet open correspond à une formation de chute libre pour toute personne répondant aux critères
+        suivants : âge minimum 15 ans révolus (autorisation écrite obligatoire des deux parents
+        pour les mineurs), âge maximum 55 ans, poids maximum 90kg, être en bonne santé et se
+        présenter le jour du saut avec un certificat médical de non contre-indication à la pratique du
+        parachutisme sportif. Ce document doit dater de moins de 180 jours le jour du saut.
+        Tenue pour sauter : jeans ou survêtement, chaussures de sport, polaire ou vêtements techniques.
+    </p>
+    <p>Ce billet est non remboursable passé le délai légal de rétractation. Sa validité est de 12 mois
+        sauf autres indications liées à une offre spéciale ou une promotion. Les offres promotionnelles
+        ne sont pas cumulables. Ce billet peut être transmis à toute autre personne répondant aux
+        critères ci-dessus. La prise de rendez-vous par téléphone ou email vaut pour réservation.
+        Prévoir 3 à 5 jours sur place selon le stage. Toute annulation (de la part du détenteur de ce
+        billet ou d’un de ses représentants) du rendez-vous moins de 8 jours avant la date de la
+        formation ou le fait de se présenter au rendez-vous en ne remplissant pas l’un des critères
+        ci-dessus entrainera la perte totale de ce billet. Aucun remboursement ni dédommagement ne sera
+        accordé. La réalisation des sauts est soumise à des impératifs météorologiques, administratifs
+        et techniques. Si l’encadrement décide que les conditions ne sont pas réunies pour effectuer le
+        saut, ce dernier sera reporté à une date ultérieure. Un report lié à la météorologie ne donne
+        pas droit à une prolongation de la date de validité. Nous vous conseillons donc de fixer une
+        date de saut dès l’achat de ce bon et de ne pas attendre la fin de la date de validité. Toute
+        formation réservée et annulée de la part du stagiaire moins de 15 jours avant le début du stage
+        est considérée comme due. Une formation entamée et non terminée est due.
+    </p>
+    <p>Moyennant le règlement d’un supplément, vous pouvez, sous certaines conditions, prolonger la
+        validit é de votre bon, pour tout renseignement : diagonalechutelibre@gmail.com
+        ou tél. +33(0)6.60.83.12.84.
+    </p>
+
+    <h2>Litiges</h2>
+    <p>Pour tout litige relatif aux présentes conditions générales de vente, seul sera compétent le
+        tribunal de commerce de Gap.
+    </p>
+    <br>
+    <br>
+    <p>
+        Les données personnelles collectées lors du processus de commande d’un billet (Nom, prénom, adresse, téléphone et email) sont utilisées uniquement pour la création et l’envoie de votre billet.
+        Ces données ne sont jamais cédées à des tiers. Elles ne sont pas communiquées.
+    </p>
+    <p>
+        Le processus d’achat sur le site <a href="www.diagonale-chute-libre.com">www.diagonale-chute-libre.com</a> est sécurisé et toutes les
+        transactions bancaires sont réalisées via Paypal. Par ailleurs le site
+        <a href="www.diagonale-chute-libre.com">www.diagonale-chute-libre.com</a>  ne conserve jamais vos informations de cartes bancaires.
+    </p>
