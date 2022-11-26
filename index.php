e018febae3bbf74ba195b2dadb9e8b997406a461
UP-4303 <up4303.pro@protonmail.com>
2022-11-26 13:17:46 +0100

    menu unstable

--- index.php
+++ index.php
@@ -3,46 +3,7 @@
 //–––––––––––––––
 
 //Include du head
-include("pages/head_index.php");
-
-?>
-<?php 
-//Deuxieme balise PHP : La page
-//–––––––––––––––
-
-
-//Header de la page index page
-$html_header = "<header id='header_index'>\n";
-
-$html_header .= "<div id='logo???'>\n";
-$html_header .= "</div>\n";
-
-$html_header .= "<span> MENU SIMPLE POUR POUVOIR CO LES PAGES AU CLIQUE : A MODFIER</span>\n";
-$html_header .= "<div id='menu_index'>\n";
-$html_header .= "<ul id='menu'>\n";
-
-$html_header .= "<li><a href='pages/parachutisme_histoire.php'>L'histoire du parachutisme</a></li>\n";
-$html_header .= "<li><a href='pages/processus_de_saut.php'>Processus de saut</a></li>\n";
-$html_header .= "<li><a href='pages/faq_tandem.php'>FAQ Tandem</a></li>\n";
-$html_header .= "<li><a href='pages/reseaux.php'>Nos réseaux</a></li>\n";
-$html_header .= "<li><a href='pages/promotion.php'>Nos promotions actuels</a></li>\n";
-$html_header .= "<li><a href='pages/prestations.php'>Prestations / Offres</a></li>\n";
-$html_header .= "<li><a href='pages/avis_clients.php'>Avis clients</a></li>\n";
-$html_header .= "<li><a href='pages/panier.php'>Panier</a></li>\n";
-$html_header .= "<li><a href='pages/confirmation_commande.php'>Confirmation de commande</a></li>\n";
-$html_header .= "<li><a href='pages/actualites.php'>Actualites</a></li>\n";
-$html_header .= "<li><a href='pages/legal_notice.php'>Mentions Légales</a></li>\n";
-$html_header .= "<li><a href='pages/place_img_in_db.php'>➜➜➜ Place Images [admin]</a></li>\n";
-$html_header .= "<li><a href='pages/CGV.php'>Conditions Générales de Vente</a></li>\n<br>";
-
-$html_header .= "<li><a href='pages/test_crud.php'>TEST</a></li>\n";
-
-$html_header .= "</ul>\n";
-$html_header .= "</div>\n";
-
-$html_header .= "</header>\n";
-
-echo($html_header);
+include("pages/head.php");
 
 ?>
 
@@ -60,6 +21,6 @@ echo($html_header);
 //–––––––––––––––
 
 //Include du footer
-include("pages/footer_index.php");
+include("pages/footer.php");
 
 ?>
--- pages/head.php
+++ pages/head.php
@@ -1,21 +1,44 @@
-<?php 
+<?php
 
-//Definissons des balises principales et liaisons aux fichiers style et main (script js).
-//Ne pas l'appeler just $html permettra de définir une variable $html directement dans les pages...
-
+// Default title
 if(!(isset($title))){
-    $title = "";
+    $title = "Diagonale Chute Libre";
 }
 
-$html_head = "<!DOCTYPE html>\n";
-$html_head .= "<html>\n<head>\n";
-$html_head .= "<meta charset='utf-8' />\n";
-$html_head .= "<title>$title</title>\n";
-$html_head .= "<link rel='stylesheet' href='../styles/style.css' />\n";
-$html_head .= "<script src='../scripts/main.js'></script>\n";
-$html_head .= "</head>\n";
-$html_head .= "<body>\n";
-
-echo($html_head);
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8' />
+    <?php
+    echo("<title>$title</title>");
+    ?>
+    <link rel='stylesheet' href='/XAMMM/styles/style.css' />
+    <script src='/XAMMM/scripts/main.js' defer></script>
+</head>
+<body>
+    <header id='header'>
+
+        <div id='logo'></div>
+        <div class='headerMenu'>
+            <span class='headerMenuTitle'>MENU</span>
+            <ul class='headerMenuList'>
+
+                <li><a href='pages/parachutisme_histoire.php'>L'histoire du parachutisme</a></li>
+                <li><a href='pages/processus_de_saut.php'>Processus de saut</a></li>
+                <li><a href='pages/faq_tandem.php'>FAQ Tandem</a></li>
+                <li><a href='pages/reseaux.php'>Nos réseaux</a></li>
+                <li><a href='pages/promotion.php'>Nos promotions actuels</a></li>
+                <li><a href='pages/prestations.php'>Prestations / Offres</a></li>
+                <li><a href='pages/avis_clients.php'>Avis clients</a></li>
+                <li><a href='pages/panier.php'>Panier</a></li>
+                <li><a href='pages/confirmation_commande.php'>Confirmation de commande</a></li>
+                <li><a href='pages/actualites.php'>Actualites</a></li>
+                <li><a href='pages/legal_notice.php'>Mentions Légales</a></li>
+                <li><a href='pages/place_img_in_db.php'>➜➜➜ Place Images [admin]</a></li>
+                <li><a href='pages/CGV.php'>Conditions Générales de Vente</a></li>
+
+                <li><a href='pages/test_crud.php'>TEST</a></li>
+            </ul>
+        </div>
+    </header>
