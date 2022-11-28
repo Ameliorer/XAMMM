b643cf928d85ed6cbff21b7c62e28819603a6da3
UP-4303 <up4303.pro@protonmail.com>
2022-11-28 08:42:02 +0100

    ?

--- pages/head.php
+++ pages/head.php
@@ -4,18 +4,16 @@
 //Ne pas l'appeler just $html permettra de définir une variable $html directement dans les pages...
 
 if(!(isset($title))){
-    $title = "";
+    $title = "Diagonale Chute Libre";
 }
-
-$html_head = "<!DOCTYPE html>\n";
-$html_head .= "<html>\n<head>\n";
-$html_head .= "<meta charset='utf-8' />\n";
-$html_head .= "<title>$title</title>\n";
-$html_head .= "<link rel='stylesheet' href='../styles/style.css' />\n";
-$html_head .= "<script src='../scripts/main.js'></script>\n";
-$html_head .= "</head>\n";
-$html_head .= "<body>\n";
-
-echo($html_head);
-
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8' />
+    <title><?php echo($title); ?></title>
+    <link rel='stylesheet' href='../styles/style.css' />
+    <script src='../scripts/main.js'></script>
+</head>
+<body>
