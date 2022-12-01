7043fc26e3ba69a21c335331deb14c56f88d8ada
UP-4303 <up4303.pro@protonmail.com>
2022-12-01 14:09:36 +0100

    merge

--- pages/head.php
+++ pages/head.php
@@ -15,8 +15,8 @@ if(!(isset($title))){
     <?php
     echo("<title>$title</title>");
     ?>
-    <link rel='stylesheet' href='/styles/style.css' />
-    <script src='/scripts/main.js' defer></script>
+    <link rel='stylesheet' href='/~XAMMM/styles/style.css' />
+    <script src='/~XAMMM/scripts/main.js' defer></script>
 </head>
 <body>
     <header id='header'>
@@ -26,21 +26,21 @@ if(!(isset($title))){
             <ul class='headerMenu'>
                 <span class='headerMenuTitle'>MENU</span>
                 <ul class='headerMenuList'>
-                    <li><a href='pages/parachutisme_histoire.php'>L'histoire du parachutisme</a></li>
-                    <li><a href='pages/processus_de_saut.php'>Processus de saut</a></li>
-                    <li><a href='pages/faq_tandem.php'>FAQ Tandem</a></li>
-                    <li><a href='pages/reseaux.php'>Nos réseaux</a></li>
-                    <li><a href='pages/promotion.php'>Nos promotions actuels</a></li>
-                    <li><a href='pages/prestations.php'>Prestations / Offres</a></li>
-                    <li><a href='pages/avis_clients.php'>Avis clients</a></li>
-                    <li><a href='pages/panier.php'>Panier</a></li>
-                    <li><a href='pages/confirmation_commande.php'>Confirmation de commande</a></li>
-                    <li><a href='pages/actualites.php'>Actualites</a></li>
-                    <li><a href='pages/legal_notice.php'>Mentions Légales</a></li>
-                    <li><a href='pages/place_img_in_db.php'>➜➜➜ Place Images [admin]</a></li>
-                    <li><a href='pages/CGV.php'>Conditions Générales de Vente</a></li>
+                    <li><a href='/~XAMMM/pages/parachutisme_histoire.php'>L'histoire du parachutisme</a></li>
+                    <li><a href='/~XAMMM/pages/processus_de_saut.php'>Processus de saut</a></li>
+                    <li><a href='/~XAMMM/pages/faq_tandem.php'>FAQ Tandem</a></li>
+                    <li><a href='/~XAMMM/pages/reseaux.php'>Nos réseaux</a></li>
+                    <li><a href='/~XAMMM/pages/promotion.php'>Nos promotions actuels</a></li>
+                    <li><a href='/~XAMMM/pages/prestations.php'>Prestations / Offres</a></li>
+                    <li><a href='/~XAMMM/pages/avis_clients.php'>Avis clients</a></li>
+                    <li><a href='/~XAMMM/pages/panier.php'>Panier</a></li>
+                    <li><a href='/~XAMMM/pages/confirmation_commande.php'>Confirmation de commande</a></li>
+                    <li><a href='/~XAMMM/pages/actualites.php'>Actualites</a></li>
+                    <li><a href='/~XAMMM/pages/legal_notice.php'>Mentions Légales</a></li>
+                    <li><a href='/~XAMMM/pages/place_img_in_db.php'>➜➜➜ Place Images [admin]</a></li>
+                    <li><a href='/~XAMMM/pages/CGV.php'>Conditions Générales de Vente</a></li>
 
-                    <li><a href='pages/test_crud.php'>TEST</a></li>
+                    <li><a href='/~XAMMM/pages/test_crud.php'>TEST</a></li>
                 </ul>
             </ul>
         </ul>
