1cb4faef9f8d32dcd3e20ab571b1508dd292a00e
Osteriux <pixave73@gmail.com>
2022-12-11 17:03:48 +0100

    panier

--- lib/gestion_panier.php
+++ lib/gestion_panier.php
@@ -10,4 +10,15 @@ if($_POST['action'] == "addToCart"){
     $crC = Createcart($conn, $_POST['idUser'], $product['id'], $_POST['dateReservation'], $_POST['today']);
     $upU = UpdateUser($conn, $_POST['idUser'], $user['lastName'], $user['firstname'], $user['mail'], $user['password'], $user['phoneNum'], $_POST['tailleUser'], $_POST['poidsUser'], $_POST['ageUser'], $user['admin']);
 }
+
+if($_POST['action'] == "retirer"){
+    Deletecart($conn, $_POST['id']);
+}
+
+if($_POST['action'] == "modifier"){
+    $today = date('Y-m-d');
+    $user = SelectUser($conn, $_POST['idUser']);
+    $upC = Updatecart($conn, $_POST['idC'], $_POST['date'], $today);
+    $upU = UpdateUser($conn, $_POST['idUser'], $user['lastName'], $user['firstname'], $user['mail'], $user['password'], $user['phoneNum'], $_POST['height'], $_POST['weight'], $_POST['age'], $user['admin']);
+}
 ?>
--- pages/confirmation_commande.php
+++ pages/confirmation_commande.php
@@ -5,16 +5,25 @@
 //Include du head
 include("head.php");
 
+if(isset($_SESSION['id'])){
+    $idUser = $_SESSION['id'];
+}else{
+    echo("<script type='text/javascript'> window.location.replace('/~XAMMM/inc/form_connection.php') </script>");
+}
+
 ?>
 
-<?php 
-//Deuxieme balise PHP : La page
-//–––––––––––––––
-
-$html_acceuil = "<div><a href='../index.php'>ACCEUIL</a></div>";
-echo($html_acceuil);
-?>
-
+<header>
+    <h1>Votre Panier</h1>
+</header>
+<main id='main_panier'>
+    <div id='aff_panier'></div>
+</main>
+<script type='text/javascript'>
+    var PHPdata = <?php echo(json_encode(array("idUser" => $idUser))); ?>;
+</script>
+<script src="../scripts/axios.min.js"></script>
+<script src="../scripts/panier.js" defer></script>
 
 <?php 
 //Troisieme balise PHP pour include le(s) fichier(s) du footer
