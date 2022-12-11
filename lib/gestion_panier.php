5905199f7414075304794abf2d0e796d4c15cc01
Osteriux <pixave73@gmail.com>
2022-12-11 11:35:30 +0100

    fix final gestion_cart.php

--- lib/gestion_panier.php
+++ lib/gestion_panier.php
@@ -1,23 +1,13 @@
 <?php
 include("../db/db_connect.php");
 include("../crud/crud_cart.php");
-include("../crud/crud_user.php");
-include("../crud/crud_product.php");
-
-$test = array("test");
-array_push($test, "ping1");
+include("../crud/crud_users.php");
+include("../crud/crud_products.php");
 
 if($_POST['action'] == "addToCart"){
-    array_push($test, "ping2");
     $product = SelectProduit($conn, $_POST['nameProduct']);
-    array_push($test, $product);
     $user = SelectUser($conn, $_POST['idUser']);
-    array_push($test, $user);
     $crC = Createcart($conn, $_POST['idUser'], $product['id'], $_POST['dateReservation'], $_POST['today']);
-    array_push($test, $crC);
-    $upU = UpdateUser($conn, $_POST['idUser'], $user['lastName'], $user['firstName'], $user['mail'], $user['password'], $user['phoneNum'], $_POST['tailleUser'], $_POST['poidsUser'], $_POST['ageUser'], $user['admin']);
-    array_push($test, $upU);
+    $upU = UpdateUser($conn, $_POST['idUser'], $user['lastName'], $user['firstname'], $user['mail'], $user['password'], $user['phoneNum'], $_POST['tailleUser'], $_POST['poidsUser'], $_POST['ageUser'], $user['admin']);
 }
-
-echo var_dump($test);
 ?>
