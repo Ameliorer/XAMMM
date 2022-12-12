df654947f03bac53cb1c642c3dc1626d6a399287
Mila Desmet <desmetmila@gmail.com>
2022-12-12 17:45:15 +0100

    remise a niveau

---
+++ pages/stagePAC.php
@@ -0,0 +1,238 @@
+
+
+
+<?php 
+//Premiere balise PHP pour includes les principaux fichiers (Head,db_connect...) ainsi que pour s'occuper des sessions (visiteur,admin)
+//–––––––––––––––
+
+//Include du head
+include("head.php");
+
+?>
+
+<?php 
+//Deuxieme balise PHP : La page
+//–––––––––––––––
+echo("<main>");
+$html_acceuil = "<div><a href='/~XAMMM/index.php'>ACCEUIL</a></div>";
+echo($html_acceuil);
+?>
+<div id=#title>
+<h1> Le saut en Tandem</h1>
+
+<p>Le saut en Parachute Tandem est la façon la plus facile est sûre de découvrir la chute libre ; le rêve d’Icare ! C’est une expérience incomparable avec tout ce que vous auriez pu  essayé avant ! Accroché à un moniteur hyper expérimenté vous avez juste à profiter de cette sensation autant intense qu’inoubliable !
+</p>
+</div>
+    
+<div id="imagesIntro">
+<a href="#preparation">
+    <img class="picture"
+        src="/~XAMMM/images/preparation.jpg"
+        alt="preparation ">
+    <p> Preparation </p>
+</a>
+
+<a href="#monteeavion">
+    <img class="picture"
+        src="/~XAMMM/images/monteeavion.jpg"
+        alt="ici une image ">
+    <p> Montee en avion </p>
+</a>
+<a href="#miseaporte">
+    <img class="picture"
+        src="/~XAMMM/images/miseaporte.jpg"
+        alt="ici une image ">
+    <p> Misa a porte </p>
+</a>
+<a href="#premieresec">
+    <img class="picture"
+        src="/~XAMMM/images/premieresec.jpg"
+        alt="ici une image ">
+    <p> Première secondes </p>
+</a>
+<a href="#chutelibre">
+    <img class="picture "
+        src="/~XAMMM/images/chutelibre.jpg"
+        alt="ici une image ">
+    <p> Chute libre </p>
+</a>
+<a href="#ouverture">
+    <img class="picture"
+        src="/~XAMMM/images/ouverture.jpg"
+        alt="ici une image ">
+    <p> Ouverture </p>
+ </a>
+<a href="#vol">
+    <img class="picture"
+        src="/~XAMMM/images/vol.jpg"
+        alt="ici une image ">
+    <p> Le vol </p>
+</a>
+<a href="#atterrissage">
+    <img class="picture"
+        src="/~XAMMM/images/atterrissage.jpg"
+        alt="ici une image ">
+    <p> Atterrissage </p>
+</a>
+<a href="#video">
+    <img class="picture"
+        src="/~XAMMM/images/video.jpg"
+        alt="ici une image ">
+    <p> Vidéo </p>
+</a>
+</div>
+
+
+
+
+
+<a id="presentation"></a>
+<div class="division">
+<h2>Presentation </h2>
+<div class="division_contenu">
+<img class="picture"
+     src="/~XAMMM/images/preparation.jpg"
+     alt="preparation ">
+<p>La PAC est un moyen moderne et rapide de permettre la pratique du parachutisme, d'explorer la chute libre et la maîtrise de soi dans la chute et le sous voile.
+Pierre et ses moniteurs sont tous des professionnels diplômés d'Etat ayant au minimum 5 000 sauts à leur actif.
+L'entraînement se fait généralement sur une semaine car le meilleur moyen de progresser est d'enchaîner les sauts.
+Vous n'avez pas de semaine complête, contactez nous pour étaler votre stage sur plusieurs week-ends .
+Attention vous serez dépendant de la météo, il est bien évident que pour des questions de sécurité personne ne saute quand il pleut ou que le vent est trop fort.
+</p>
+</div>
+</div>
+
+
+<a id="formation"></a>
+<div class="division">
+<h2>La formation</h2>
+
+<div class="division_contenu">
+
+<img class="picture"
+     src="/~XAMMM/images/monteeavion.jpg"
+     alt="ici une image ">
+
+<p>Une formation d'une demi-journée au cours de laquelle on vous expliquera : les différents éléments d'un parachute, les règles de sécurité, la methode de conduite sous voile, la position en chute libre et les conseils pour une pratique éthique.
+</p>
+</div>
+</div>
+
+
+<a id="equipement"></a>
+<div class="division ">
+<h2>L'equipement</h2>
+
+<div class="division_contenu ">
+
+<img class="picture"
+     src="/~XAMMM/images/miseaporte.jpg"
+     alt="ici une image ">
+<p>Une fois le feu vert du pilote, les parachutistes confirmés quitteront l'avion en premier. Puis ce sera à vous ! Vous approcherez de la porte avec votre moniteur pour vous asseoir au sol (Mains sur le harnais et tête en arrière) Une dernière inspiration et votre moniteur donnera l’impulsion qui vous ejectera dans le nouvel espace de jeu le CIEL !</p>
+</div>
+</div>
+
+
+
+<a id="premiersaut"></a>
+<div class="division">
+<h2>Le premier saut </h2>
+
+<div class="division_contenu">
+
+<img class="picture"
+     src="/~XAMMM/images/premieresec.jpg"
+     alt="ici une image ">
+<p>Le premier saut se déroule depuis une hauteur de 4000 m avec deux moniteurs qui vous suivront durant toute chute libre.
+Un moniteur sera devant vous pour communiquer au moyen de signes appris durant la formation.
+Un autre reste sur le coté en vous tenant afin de maintenir votre stabilité.
+Sous voile, vous serez guidé par radio.
+Après l'atterrissage, un débrief vidéo est fait  pour vous préparer au prochain saut.
+!</p>
+</div>
+</div>
+
+
+<a id="chutelibre"></a>
+<div class="division">
+<h2>La chute libre</h2>
+<div class="division_contenu">
+
+<img class="picture "
+     src="/~XAMMM/images/chutelibre.jpg"
+     alt="ici une image ">
+
+<p>2500 mètres de chute libre à 200 kilomètres heure ça fait environ  50 secondes de chute libre ! N’oubliez pas de respirer l’air pur des hautes alpes, ouvrez grand les yeux profitez de ce sentiment de flotter dans les airs ! A cette vitesse le vent relatif est un fluide ! Souriez ! Regardez le superbe paysage ! Réalisez que vous êtes en train de faire un saut en parachute depuis 4000 mètres ! Kiffez ! Profitez de la minute de votre vie !</p>
+</div>
+</div>
+
+
+<a id="ouverture"></a>
+<div class="division">
+<h2>L’ouverture du parachute</h2>
+
+<div class="division_contenu">
+
+<img class="picture"
+     src="/~XAMMM/images/ouverture.jpg"
+     alt="ici une image ">
+<p>A 1500 mètres le moniteur déclenchera l'ouverture du parachute. Votre voile commencera à prendre l'air, pour s’ouvrir progressivement en quelques secondes durant lesquelles notre vitesse passera de 200 km/h à 20 km/h. L’ouverture n’est pourtant pas brutale : rassurez vous !</p>
+</div>
+</div>
+
+
+<a id="vol"></a>
+<div class="division">
+<h2>Le vol sous le parachute</h2>
+
+<div class="division_contenu">
+
+<img class="picture"
+     src="/~XAMMM/images/vol.jpg"
+     alt="ici une image ">
+
+<p>Après l’ouverture le plus confortable est de décompresser comme en plongée sous marine (manœuvre de Valsalva). Le moniteur vous montrera comment piloter le parachute ! Il vous invitera a retirer vos lunettes de chute pour plus de confort vous demandera vos impressions souvent raconté de façon très expressive ! Ensuite vous vous entrainerez à prendre la position pour un atterrissage sans encombres !</p>
+</div>
+</div>
+
+
+<a id="atterrissage"></a>
+<div class="division">
+<h2>L'atterrissage</h2>
+
+<div class="division_contenu">
+
+<img class="picture"
+     src="/~XAMMM/images/atterrissage.jpg"
+     alt="ici une image ">
+<p>Le moniteur effectuera la manœuvre d’arrondi qui permet de se poser en douceur ; votre rôle consistera à bien lever les jambes pour que ce que ce soit celles du moniteur qui touche le sol en premier pour éviter tout choc ! </p>
+</div>
+</div>
+
+
+<a id="video"></a>
+<div class="division">
+<h2>La vidéo embarquée</h2>
+<div class="division_contenu">
+
+<img class="picture"
+     src="/~XAMMM/images/video.jpg"
+     alt="ici une image ">
+
+<p>Le moniteur filmera toute la préparation du saut, la montée en avion et le paysage, la mise en place à la porte, toute la chute libre ou vous serez  filmé en gros plan. Après l’ouverture le moniteur vous interviewera sous voile en direct live ! Puis vos dédicaces éventuelles et vos impressions une fois posé ! Le tout est filmé en HD et vous sera remis sur une clef USB !</p>
+</div>
+</div>
+</main>
+
+
+
+<script src="/~XAMMM/scripts/processus_saut.js"></script>
+
+<?php 
+//Troisieme balise PHP pour include le(s) fichier(s) du footer
+//–––––––––––––––
+
+//Include du footer
+include("footer.php");
+
+?>
