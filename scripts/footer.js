c9332ce7281a10b28713ca649485e9a130f7eb55
UP-4303 <up4303.pro@protonmail.com>
2022-12-12 17:02:03 +0100

    Revert "test footer"
    This reverts commit 4833bf2d240812db389a222555da842d167bd61e.

--- scripts/footer.js
+++ scripts/footer.js
@@ -47,7 +47,7 @@ axios.get("/~XAMMM/lib/opinion.php?opinionAction=5").then(Response => {
     });
 });
 
-screen.addEventListener('load', event => {
+window.onload = function(){
     var slides = document.getElementsByClassName('carousel-item'),
         addActive = function(slide) {slide.classList.add('active')},
         addUnactive = function(slide) {slide.classList.add('unactive')},
@@ -81,4 +81,5 @@ screen.addEventListener('load', event => {
 
         }
     }, 7000);
-})
+}
+
