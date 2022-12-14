6e922b3f34f959c7185480bcfa7a75d35596f16e
Amélie HACQUE <95433250+Ameliorer@users.noreply.github.com>
2022-12-14 01:38:44 +0100

    grammaire

--- pages/parachutisme_histoire.php
+++ pages/parachutisme_histoire.php
@@ -16,14 +16,14 @@ include("head.php");
     <img class="picture"
         src="/~XAMMM/images/intro.jpg"
         alt="intro ">
-    <p> Definition </p>
+    <p> Définition </p>
 </a>
 
 <a href="#vinci">
     <img class="picture"
         src="/~XAMMM/images/vinci.png"
         alt="ici une image ">
-    <p> Le parchute de vinci </p>
+    <p> Le parachute de vinci </p>
 </a>
 <a href="#garnerin">
     <img class="picture"
@@ -47,7 +47,7 @@ include("head.php");
     <img class="picture"
         src="/~XAMMM/images/securite.png"
         alt="ici une image ">
-    <p> Securite </p>
+    <p> Sécurité </p>
  </a>
 <a href="#avenir">
     <img class="picture"
@@ -67,7 +67,7 @@ include("head.php");
      src="/~XAMMM/images/intro.jpg"
      alt="image ">
  <p>Le parachute est un dispositif destiné à freiner le mouvement, principalement vertical d'un objet ou d'un individu dans les airs. Historiquement, le parachute sert à rendre possible le retour au sol en bonne condition d'une personne abandonnant un aéronef, mais le terme a été appliqué à de nombreux dispositifs destinés à empêcher une chute ou à ralentir un appareil au moyen d'un dispositif semblable à un parachute de descente.
- <span>Etymologie :</span> Le mot parachute est constitué du préfixe para- (du latin paro (« parer, contrer »)) et du mot français chute (fait de tomber) : protection contre la chute. Le mot a été inventé par Louis-Sébastien Lenormand en 1785.</p>
+ <span>Étymologie :</span> Le mot parachute est constitué du préfixe para- (du latin paro (« parer, contrer »)) et du mot français chute (fait de tomber) : protection contre la chute. Le mot a été inventé par Louis-Sébastien Lenormand en 1785.</p>
 
 </div>
 </div>
@@ -82,7 +82,7 @@ include("head.php");
      <p> 
 Le parachute a été conçu par Léonard de Vinci en 1485.
 Il s'agit d'un parachute pyramidal d'une hauteur latérale d'environ 7 mètres.
-Ce n'est qu'en 2008 que le premier saut avec une réplique du parachute de Léonard de Vinci a été réalisé par le parachutiste suisse Olivier Vietti-Teppa .Largué en hélicoptère d'une hauteur de 650 mètres, parachuté à 600 mètres et il a atterri sans encombre.
+Ce n'est qu'en 2008 que le premier saut avec une réplique du parachute de Léonard de Vinci a été réalisé par le parachutiste suisse Olivier Vietti-Teppa. Largué en hélicoptère d'une hauteur de 650 mètres, parachuté à 600 mètres et il a atterri sans encombre.
 </p> 
 </div>
 </div>
@@ -95,7 +95,7 @@ Ce n'est qu'en 2008 que le premier saut avec une réplique du parachute de Léon
      src="/~XAMMM/images/garnerin.png"
      alt="image ">
 <p>
-Le premier saut par Garnerin
+Le premier saut par Garnerin.
 Le premier saut en parachute a été effectué en 1797 par André Jacques Garnerin au-dessus d'un parc à Paris.
 Puis un siècle s'est déroulé avant que le parachute ne soit utilisé.
 Ce n'est qu'à partir des années 1940 qu'il se développe significativement puis devient un sport.
@@ -144,7 +144,7 @@ C'est ainsi que les nouvelles disciplines ont vu : Vol Relatif, Freestyle et Fre
      alt="image ">
      <p> 
 Aujourd’hui la Fédération Française de Parachutisme compte plus de 7000 adhérents réguliers.
-Le parachutisme est un sport en pleine expansion.Nous pouvons par exemple compter  plus de 22 000 sauts de découverte et de débutants par an.
+Le parachutisme est un sport en pleine expansion. Nous pouvons par exemple compter plus de 22 000 sauts de découverte et de débutants par an.
 Cette augmentation du nombre de participants va de pair avec le développement important des mesures de sécurité.
 </p>      
 </div>
@@ -161,7 +161,7 @@ Cette augmentation du nombre de participants va de pair avec le développement i
      alt="image ">
 <p> 
 Le parachutisme n'est plus une activité extrême réservée aux marginalisés, mais un sport à part entière.
-Ceci est soutenu par une étude portant sur l'intégration des disciplines olympiques et la réalisation de sauts de démonstration aux Jeux olympiques de 2000 à Sidney.
+Ceci est soutenu par une étude portant sur l'intégration des disciplines olympiques et la réalisation de sauts de démonstration aux Jeux Olympiques de 2000 à Sidney.
 
 Depuis 2008, des parachutistes débarquent sur les Champs Élysées le 14 juillet.
 Ces images, vues par des millions de personnes, donnent au sport une image très positive et le valorisent ainsi.
